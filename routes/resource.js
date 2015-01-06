34ea0da44a42ba6496c1ffe16755018a987e19a2
Ryan Jones <ryan.jones26@gmail.com>
2015-01-05 21:48:07 -0500

    dist-9 complete

--- routes/resource.js
+++ routes/resource.js
@@ -18,7 +18,34 @@ router.get('/', function(req, res,next) {
 router.post('/', function(req, res, next) {
     Resource.create(req.body, function(err, post) {
         if(err) return next(err);
+        res.json(post);
     });
 });
 
+// get by id
+
+router.get('/:id', function(req, res, next) {
+    Resource.findById(req.params.id, function(err, post){
+        if(err) return next(err);
+        res.json(post);
+    });
+});
+
+// update
+
+router.put('/:id', function(req, res, next) {
+    Resource.findByIdAndUpadte(req.params.id, req.body, function(err, post) {
+        if(err) return next(err);
+        res.jsoon(post)
+    });
+});
+
+// delete
+
+router.delete('/:id', function(req, res, next){
+    Resource.findByIdAndRemove(req.params.id, req.body, function(err, post){
+        if(err) return next(err);
+        res.json(post);
+    });
+});
 module.exports = router;
